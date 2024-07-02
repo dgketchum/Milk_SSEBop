68619fa12611ce940268ae00bbce370c6889d924
dgketchum <dgketchum@gmail.com>
2024-07-02 17:52:53 -0600

    write timeseries for three land cover classes

--- ee_api/call_ee.py
+++ ee_api/call_ee.py
@@ -3,9 +3,8 @@ import sys
 from calendar import monthrange
 
 import ee
-import numpy as np
 
-from ee_api.landcover import get_crop_cover
+from ee_api.landcover import get_landcover
 
 sys.path.insert(0, os.path.abspath('..'))
 sys.setrecursionlimit(5000)
@@ -14,7 +13,48 @@ ET_COLLECTION = 'projects/dri-milkriver/assets/ssebop/nldas/monthly/v0_0'
 SMM_DISSOLVE = 'projects/ee-dgketchum/assets/milk/smm_dissolve'
 
 
-def export_gridded_data(tables, bucket, years, description, debug=False, join_col='STAID', volumes=False):
+def export_geo():
+    geo = ee.Geometry.Polygon([
+        [-113.75846106339121, 48.428983885591904],
+        [-111.41837317276621, 48.428983885591904],
+        [-111.41837317276621, 48.983607830957894],
+        [-113.75846106339121, 48.983607830957894],
+        [-113.75846106339121, 48.428983885591904],
+    ])
+    return geo
+
+def get_test_fc():
+    ag = ee.Geometry.Polygon([
+        [-112.6565977459894, 48.68663591289309],
+        [-112.6232954388605, 48.68663591289309],
+        [-112.6232954388605, 48.70261246685249],
+        [-112.6565977459894, 48.70261246685249],
+        [-112.6565977459894, 48.68663591289309],
+    ])
+
+    grass = ee.Geometry.Polygon([
+        [-112.86539981088687, 48.49227169041395],
+        [-112.74695346078921, 48.49227169041395],
+        [-112.74695346078921, 48.52457026274042],
+        [-112.86539981088687, 48.52457026274042],
+        [-112.86539981088687, 48.49227169041395],
+    ])
+
+    forest = ee.Geometry.Polygon([
+        [-113.69254309464121, 48.89612942802007],
+        [-113.44947058487558, 48.89612942802007],
+        [-113.44947058487558, 48.96605036549195],
+        [-113.69254309464121, 48.96605036549195],
+        [-113.69254309464121, 48.89612942802007],
+    ])
+
+    features = [ee.Feature(g, {'FID': c}) for g, c in zip([ag, grass, forest], [1, 2, 3])]
+    fc = ee.FeatureCollection(features)
+
+    return fc
+
+
+def export_gridded_data(tables, bucket, years, description, debug=False, clip_fc=None, join_col='STAID', **kwargs):
     """
     Reduce Regions, i.e. zonal stats: takes a statistic from a raster within the bounds of a vector.
     Use this to get e.g. irrigated area within a county, HUC, or state. This can mask based on Crop Data Layer,
@@ -24,49 +64,71 @@ def export_gridded_data(tables, bucket, years, description, debug=False, join_co
     ee.Initialize()
     fc = ee.FeatureCollection(tables)
 
-    clip = ee.FeatureCollection(SMM_DISSOLVE)
+    if clip_fc:
+        clip = ee.FeatureCollection(clip_fc)
+    else:
+        clip = None
+
+    if not kwargs:
+        target_classes = [1]
+    else:
+        target_classes = kwargs['target_classes']
+
+    area = ee.Image.pixelArea()
 
     for yr in years:
         for month in range(1, 13):
+
+            bands = None
 
             s = '{}-{}-01'.format(yr, str(month).rjust(2, '0'))
             end_day = monthrange(yr, month)[1]
             e = '{}-{}-{}'.format(yr, str(month).rjust(2, '0'), end_day)
 
-            et = ee.ImageCollection(ET_COLLECTION).filterDate(s, e).select('et').mosaic().clip(clip)
-            et = et.reproject(crs='EPSG:5070', scale=30).resample('bilinear').rename('et')
+            if clip:
+                et = ee.ImageCollection(ET_COLLECTION).filterDate(s, e).select('et').mosaic().clip(clip)
+            else:
+                et = ee.ImageCollection(ET_COLLECTION).filterDate(s, e).select('et').mosaic()
 
-            crops = get_crop_cover(yr).clip(clip)
-            crops = crops.reproject(crs='EPSG:5070', scale=30).resample('bilinear').rename('crops')
+            et = et.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
 
-            crop_mask = crops.lt(1)
-            et = et.mask(crop_mask)
+            if clip:
+                lc_band = get_landcover(yr).clip(clip)
+            else:
+                lc_band = get_landcover(yr)
 
-            area = ee.Image.pixelArea()
+            lc_band = lc_band.reproject(crs='EPSG:5070', scale=30).resample('bilinear')
 
-            if volumes:
-                crop_area = crops.multiply(area)
-                et = et.multiply(area)
+            first, select_ = True, None
 
-            bands = et.addBands([crop_area])
-            select_ = [join_col, 'et', 'crops']
+            for target_class in target_classes:
+
+                lc_mask = lc_band.eq(target_class)
+                lc_name = 'lc_{}'.format(target_class)
+                lc_area = lc_band.mask(lc_mask).multiply(area).rename(lc_name).divide(1e6)
+
+                et_band = et.mask(lc_mask)
+                et_name = 'et_{}'.format(target_class)
+                et_area = et_band.multiply(area).rename(et_name).divide(1e9)
+
+                if first:
+                    bands = et_area.addBands([lc_area])
+                    select_ = [join_col, lc_name, et_name]
+                    first = False
+                else:
+                    bands = bands.addBands([et_area, lc_area])
+                    [select_.append(b) for b in [lc_name, et_name]]
 
             if debug:
-                samp = fc.filterMetadata('FID', 'equals', 0).geometry()
-                field = bands.reduceRegions(collection=samp,
-                                            reducer=ee.Reducer.sum(),
-                                            scale=30)
-                p = field.first().getInfo()['properties']
-                print('{} propeteries {}'.format(yr, p))
-
-            if volumes:
+                fc = get_test_fc()
                 data = bands.reduceRegions(collection=fc,
                                            reducer=ee.Reducer.sum(),
                                            scale=30)
-            else:
-                data = bands.reduceRegions(collection=fc,
-                                           reducer=ee.Reducer.mean(),
-                                           scale=30)
+                info = data.getInfo()
+
+            data = bands.reduceRegions(collection=fc,
+                                       reducer=ee.Reducer.sum(),
+                                       scale=30)
 
             out_desc = '{}_{}_{}'.format(description, yr, month)
             task = ee.batch.Export.table.toCloudStorage(
@@ -85,7 +147,7 @@ if __name__ == '__main__':
     ee.Authenticate()
     ee.Initialize(project='ssebop-montana')
 
-    export_gridded_data(SMM_DISSOLVE, 'wudr', years=[i for i in range(2022, 2024)],
-                        description='smm_crop_et', debug=False, join_col='FID', volumes=True)
+    export_gridded_data(SMM_DISSOLVE, 'wudr', years=[i for i in range(1985, 2024)],
+                        description='smm_lc_et', debug=False, join_col='FID', **{'target_classes': [1, 2, 3]})
 
 # ========================= EOF ================================================================================
--- timeseries.py
+++ timeseries.py
@@ -13,18 +13,21 @@ from bokeh.plotting import figure, output_file, save
 def compile_data(csv_dir, plot_dir):
     l = [os.path.join(csv_dir, x) for x in os.listdir(csv_dir)]
 
-    first = True
-    dates, et, crops = [], [], []
+    first, df = True, None
     for f in l:
-        c = pd.read_csv(f)
-        # smm_crop_et_1985_1.csv
-        splt = os.path.basename(f).split('.')[0].split('_')[-2:]
-        dt = '{}-{}-01'.format(splt[0], splt[1].rjust(2, '0'))
-        dates.append(dt)
-        et.append(c.loc[0, 'et'] / 1e9)
-        crops.append(c.loc[0, 'crops'] / 1e6)
+        splt = os.path.basename(f).split('.')[0].split('_')
+        dt = pd.to_datetime('{}-{}-01'.format(splt[-2], splt[-1]))
+        if first:
+            df = pd.read_csv(f)
+            df['date'] = dt
+            first = False
+        else:
+            c = pd.read_csv(f)
+            c['date'] = dt
+            df = pd.concat([df, c], ignore_index=False)
 
-    df = pd.DataFrame(index=pd.DatetimeIndex(dates), data=np.array([et, crops]).T, columns=['et', 'crops'])
+    df.index = pd.DatetimeIndex(df['date'])
+    df.drop(columns=['FID'])
     df = df.sort_index()
 
     plots = []
@@ -33,16 +36,9 @@ def compile_data(csv_dir, plot_dir):
     p = figure(title='ET', x_axis_label='Time', y_axis_label='Value', width=2400, height=800,
                x_axis_type="datetime")
 
-    p.line(df.index, df['et'].values, line_width=0.5, color='blue', legend_label='Prior')
+    for lc, key in zip(['Agriculture', 'Grass/Shrubland', 'Forest'], [1, 2, 3]):
 
-    p.legend.location = "top_left"
-    p.xaxis.formatter = DatetimeTickFormatter(days=["%Y-%m-%d"], months=["%Y-%m-%d"], years=["%Y-%m-%d"])
-    plots.append(p)
-
-    p = figure(title='Cropped Area', x_axis_label='Time', y_axis_label='Area [km 2]', width=2400, height=800,
-               x_axis_type="datetime")
-
-    p.line(df.index, df['crops'].values, line_width=0.5, color='green', legend_label='Prior')
+        p.line(df.index, df['et_{}'.format(key)].values, line_width=1.0, color=colors[key], legend_label=lc)
 
     p.legend.location = "top_left"
     p.xaxis.formatter = DatetimeTickFormatter(days=["%Y-%m-%d"], months=["%Y-%m-%d"], years=["%Y-%m-%d"])
