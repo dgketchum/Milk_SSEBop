fc6c9c2086b54a78ce774d4a0cac0401b4dcb815
dgketchum <dgketchum@gmail.com>
2025-02-12 12:57:39 -0700

    finished first draft empirical copula sampling

--- eto_monte_carlo.py
+++ eto_monte_carlo.py
@@ -6,13 +6,9 @@ from pprint import pprint
 
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
-import statsmodels.api as sm
 import torch
-from plotly.subplots import make_subplots
 from refet import Daily
 from scipy.stats import rankdata
-from statsmodels.tsa.stattools import ccf
 
 from eto_error import COMPARISON_VARS
 from eto_error import station_par_map
@@ -29,8 +25,8 @@ def mc_timeseries_draw(station_meta, gridded, outdir, residuals_dir, station_typ
 
     for j, (station, row) in enumerate(station_list.iterrows()):
 
-        if station not in ['bftm', 'comt', 'bfam']:
-            continue
+        # if station not in ['bftm', 'comt', 'bfam']:
+        #     continue
 
         outfile = os.path.join(outdir, f'eto_variance_{num_samples}_{station}.json')
 
@@ -56,7 +52,7 @@ def mc_timeseries_draw(station_meta, gridded, outdir, residuals_dir, station_typ
         res_df = pd.read_csv(resid_file, parse_dates=True, index_col='date')
         res_df.index = pd.DatetimeIndex([datetime.date(i.year, i.month, i.day) for i in res_df.index])
         res_df.dropna(how='any', axis=0, inplace=True)
-        res_df['year'] = [i.year for i in res_df.index]
+        res_df['year'] = res_df.index.year
         res_df = res_df.rename(columns={'u2': 'wind', 'tmean': 'mean_temp'})
         residuals = res_df[metvars].values
 
@@ -64,70 +60,80 @@ def mc_timeseries_draw(station_meta, gridded, outdir, residuals_dir, station_typ
         eto_arr = gdf.loc[match_idx, 'eto'].values
 
         gdf = gdf.loc[match_idx]
-        model_estimates = gdf[metvars].values
+        doy = gdf.index.dayofyear
+
+        # this ordering is critical
+        addnl_vars = ['ea', 'rsds', 'eto']
+        model_estimates = gdf[metvars + addnl_vars].values
 
         result = {k: [] for k in metvars}
         result['stats'] = {var: {} for var in metvars}
 
-        perturbed_model_data = correlated_residual_sampling(residuals, model_estimates, num_samples)
+        pert_data, unmod_data = correlated_residual_sampling(residuals, model_estimates, num_samples)
 
         for var_idx, var in enumerate(metvars):
-            for i in range(num_samples):
-                sim_df = gdf.copy()
-                sim_df[var] = perturbed_model_data[i, :, var_idx]
 
-                sim_dw = sm.stats.durbin_watson(sim_df[var])
-                obs_dw = sm.stats.durbin_watson(gdf[var])
+            sim = np.zeros((num_samples, 1 + len(metvars) + len(addnl_vars)))
 
-                gdf_mean, sim_mean = gdf[var].mean(), sim_df[var].mean()
+            sim_idx = np.random.choice(np.arange(num_samples), size=num_samples, replace=True)
+            date_idx = np.random.choice(np.arange(residuals.shape[0]), size=num_samples, replace=True)
 
-                if i == 0:
-                    print('Durbin-Watson {}: obs {:.2f}, sim {:.2f}'.format(var, obs_dw, sim_dw))
-                    print('Mean Residuals {}: obs {:.2f}, sim {:.2f}'.format(var, gdf_mean, sim_mean))
-                    result['stats'][var] = {'obs_dw': [], 'sim_dw': [], 'obs_mean_res': [], 'sim_mean_res': []}
+            for e, (i, j) in enumerate(zip(sim_idx, date_idx)):
+                sim[e, 0] = doy[j]
+                sim[e, 1:] = unmod_data[i, j, :]
+                sim[e, var_idx + 1] = pert_data[i, j, var_idx]
 
-                    fig = make_subplots(rows=len(metvars), cols=1, shared_xaxes=True, subplot_titles=metvars)
-                    for k, v in enumerate(metvars):
-                        fig.add_trace(go.Scatter(x=gdf.index[-365:], y=gdf[v][-365:], mode='lines', name='Observed',
-                                                 line=dict(color='blue')), row=k + 1, col=1)
-                        fig.add_trace(go.Scatter(x=sim_df.index[-365:], y=sim_df[v][-365:], mode='lines',
-                                                 name='Synthesized', line=dict(color='red')), row=k + 1, col=1)
-                    fig.update_layout(height=800, width=1000, showlegend=True)
-                    fig.show()
+            gdf_mean, sim_mean = gdf[var].mean(), sim[:, var_idx + 1].mean()
 
-                result['stats'][var]['obs_dw'].append(obs_dw)
-                result['stats'][var]['sim_dw'].append(sim_dw)
-                result['stats'][var]['obs_mean_res'].append(gdf_mean)
-                result['stats'][var]['sim_mean_res'].append(sim_mean)
+            print('Mean Residuals {}: obs {:.2f}, sim {:.2f}'.format(var, gdf_mean, sim_mean))
+            result['stats'][var] = {'obs_mean_res': [], 'sim_mean_res': []}
 
-                eto_sim = sim_df.apply(calc_eto, mod_var=var, elev=row[kw['elev']], lat=row[kw['lat']],
-                                       mod_vals=sim_df[var].values, axis=1).values
+            result['stats'][var]['obs_mean_res'].append(gdf_mean)
+            result['stats'][var]['sim_mean_res'].append(sim_mean)
 
-                eto_sim = pd.Series(data=eto_sim, index=gdf.index)
-                eto_sim = eto_sim.loc[match_idx].values
+            sim_df = pd.DataFrame(data=sim, columns=['doy'] + metvars + addnl_vars)
 
-                res = eto_sim - eto_arr
-                variance = np.var(res, ddof=1)
-                result[var].append((res.mean(), variance))
+            # duplicate 'mean_temp' to 'min_temp' and 'max_temp' for convenience
+            sim_df['min_temp'] = sim_df['mean_temp'].copy()
+            sim_df['max_temp'] = sim_df['mean_temp'].copy()
 
-                mean_ = res.mean()
-                if i == 0:
-                    print('Mean ETo residual for {}: {:.2f}\n'.format(var, mean_))
+            eto_sim = sim_df.apply(calc_eto, mod_var=var, elev=row[kw['elev']], lat=row[kw['lat']],
+                                   mod_vals=sim_df[var].values, axis=1).values
+
+            sim_df['eto_sim'] = eto_sim
+
+            sim_df['res'] = sim_df['eto'] - sim_df['eto_sim']
+
+            res = sim_df['res'].values
+
+            variance = np.var(res, ddof=1)
+            result[var].append((res.mean(), variance))
+
+            mean_ = res.mean()
+            print('Mean ETo residual for {}: {:.2f}\n'.format(var, mean_))
 
         with open(outfile, 'w') as f:
             json.dump(result, f, indent=4)
 
 
 def correlated_residual_sampling(residuals, model_estimates, n_samples):
+    """"""
     n_time, n_vars = residuals.shape
-    perturbed_estimates = np.zeros((n_samples, n_time, n_vars))
+
+    perturbed_estimates = np.zeros((n_samples, n_time, model_estimates.shape[1]))
+    unperturbed_estimates = np.zeros((n_samples, n_time, model_estimates.shape[1]))
+
     correlated_residual_samples = empirical_copula_sample(residuals, n_samples)
     for i in range(n_samples):
         for j in range(n_vars):
             perturbed_data = model_estimates.copy()
+
+            unperturbed_estimates[i, :, :] = perturbed_data
+
             perturbed_data[:, j] = model_estimates[:, j] + correlated_residual_samples[i, j]
             perturbed_estimates[i, :, :] = perturbed_data
-    return perturbed_estimates
+
+    return perturbed_estimates, unperturbed_estimates
 
 
 def empirical_copula_sample(data, n_samples):
@@ -143,36 +149,6 @@ def empirical_copula_sample(data, n_samples):
             idx = np.argmin(np.abs(rankdata(data[:, j]) / (n_time_steps + 1) - uniform_samples[i, j]))
             samples[i, j] = data[idx, j]
     return samples
-
-
-def cross_autocorrelation_multivariate(df, lags=10, demean=True):
-    if not isinstance(df, pd.DataFrame):
-        return {}
-
-    if df.empty:
-        return {}
-
-    series_names = df.columns
-    n_series = len(series_names)
-    results = {}
-
-    for i in range(n_series):
-        for j in range(n_series):
-            name_i = series_names[i]
-            name_j = series_names[j]
-            series_i = df[name_i]
-            series_j = df[name_j]
-
-            if demean:
-                series_i = series_i - series_i.mean()
-                series_j = series_j - series_j.mean()
-
-            correlation = ccf(series_i, series_j, adjusted=False)[:lags + 1]
-            lags_used = np.arange(lags + 1)
-
-            results[(name_i, name_j)] = pd.DataFrame(index=lags_used, data={"correlation": correlation})
-
-    return results
 
 
 def variance_decomposition(sim_dir, station_meta, decomp_out, station_type='ec', n_sample=100):
@@ -259,13 +235,13 @@ if __name__ == '__main__':
 
     # pandarallel.initialize(nb_workers=10)
 
-    num_sampl_ = 1000
+    num_sampl_ = 10000
     variance_json = os.path.join(d, 'weather_station_data_processing', 'error_analysis', 'mc_par_variance')
     res_file_dir = os.path.join(d, 'weather_station_data_processing', 'comparison_data')
     mc_timeseries_draw(sta, grid_data, variance_json, res_file_dir, station_type='agri',
                        num_samples=num_sampl_, overwrite=True)
 
     decomp = os.path.join(d, 'weather_station_data_processing', 'error_analysis', 'var_decomp_par.csv')
-    # variance_decomposition(variance_json, sta, decomp, station_type='agri', n_sample=num_sampl_)
+    variance_decomposition(variance_json, sta, decomp, station_type='agri', n_sample=num_sampl_)
 
 # ========================= EOF ====================================================================
