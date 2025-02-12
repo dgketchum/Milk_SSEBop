5a83f2c9f3b1214ceba01f59542bbfb1728233e3
dgketchum <dgketchum@gmail.com>
2025-02-11 17:08:55 -0700

    implementing copula-based approach

--- eto_monte_carlo.py
+++ eto_monte_carlo.py
@@ -6,11 +6,12 @@ from pprint import pprint
 
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 import statsmodels.api as sm
 import torch
+from plotly.subplots import make_subplots
 from refet import Daily
-from sdv.metadata import Metadata
-from sdv.sequential import PARSynthesizer
+from scipy.stats import rankdata
 from statsmodels.tsa.stattools import ccf
 
 from eto_error import COMPARISON_VARS
@@ -24,11 +25,12 @@ def mc_timeseries_draw(station_meta, gridded, outdir, residuals_dir, station_typ
                        overwrite=False):
     kw = station_par_map(station_type)
 
-    results, metadata, first = {}, None, True
-
     station_list = pd.read_csv(station_meta, index_col=kw['index'])
 
     for j, (station, row) in enumerate(station_list.iterrows()):
+
+        if station not in ['bftm', 'comt', 'bfam']:
+            continue
 
         outfile = os.path.join(outdir, f'eto_variance_{num_samples}_{station}.json')
 
@@ -47,16 +49,6 @@ def mc_timeseries_draw(station_meta, gridded, outdir, residuals_dir, station_typ
         gdf['mean_temp'] = (gdf['min_temp'] + gdf['max_temp']) / 2
         metvars = COMPARISON_VARS[:4]
 
-        metadata = Metadata.detect_from_dataframes(data={'met_data': gdf[metvars + ['year']]})
-        metadata.update_column('year', sdtype='id')
-        metadata.set_sequence_key('year')
-        metadata.validate()
-
-        metadata_file = os.path.join(outdir, f'metadata_{station}.json')
-        if os.path.exists(metadata_file):
-            os.remove(metadata_file)
-        metadata.save_to_json(metadata_file)
-
         resid_file = os.path.join(residuals_dir, 'res_{}.csv'.format(station))
         if not os.path.isfile(resid_file):
             continue
@@ -66,35 +58,23 @@ def mc_timeseries_draw(station_meta, gridded, outdir, residuals_dir, station_typ
         res_df.dropna(how='any', axis=0, inplace=True)
         res_df['year'] = [i.year for i in res_df.index]
         res_df = res_df.rename(columns={'u2': 'wind', 'tmean': 'mean_temp'})
+        residuals = res_df[metvars].values
 
         match_idx = [i for i in res_df.index if i in gdf.index]
         eto_arr = gdf.loc[match_idx, 'eto'].values
 
-        gdf = gdf.loc[res_df.index[0]: res_df.index[-1]]
-
-        pct_anom = res_df.copy()
-        for var in metvars:
-            pct_anom[var] = 1 + (res_df.loc[match_idx, var] / gdf.loc[match_idx, var])
+        gdf = gdf.loc[match_idx]
+        model_estimates = gdf[metvars].values
 
         result = {k: [] for k in metvars}
         result['stats'] = {var: {} for var in metvars}
 
-        pct_anom = pct_anom.replace([np.inf, -np.inf], np.nan)
-        pct_anom = pct_anom.dropna()
+        perturbed_model_data = correlated_residual_sampling(residuals, model_estimates, num_samples)
 
-        synthesizer = PARSynthesizer(metadata, cuda=True, verbose=True, epochs=128, enforce_min_max_values=True)
-        synthesizer.fit(pct_anom[metvars + ['year']])
-
-        for i in range(num_samples):
-
-            synthetic_anomalies = synthesizer.sample(num_sequences=1, sequence_length=gdf.shape[0])
-            synthetic_anomalies['doy'] = gdf['doy']
-            synthetic_anomalies.index = gdf.index
-
-            for var in metvars:
-
+        for var_idx, var in enumerate(metvars):
+            for i in range(num_samples):
                 sim_df = gdf.copy()
-                sim_df[var] *= synthetic_anomalies[var]
+                sim_df[var] = perturbed_model_data[i, :, var_idx]
 
                 sim_dw = sm.stats.durbin_watson(sim_df[var])
                 obs_dw = sm.stats.durbin_watson(gdf[var])
@@ -105,6 +85,15 @@ def mc_timeseries_draw(station_meta, gridded, outdir, residuals_dir, station_typ
                     print('Durbin-Watson {}: obs {:.2f}, sim {:.2f}'.format(var, obs_dw, sim_dw))
                     print('Mean Residuals {}: obs {:.2f}, sim {:.2f}'.format(var, gdf_mean, sim_mean))
                     result['stats'][var] = {'obs_dw': [], 'sim_dw': [], 'obs_mean_res': [], 'sim_mean_res': []}
+
+                    fig = make_subplots(rows=len(metvars), cols=1, shared_xaxes=True, subplot_titles=metvars)
+                    for k, v in enumerate(metvars):
+                        fig.add_trace(go.Scatter(x=gdf.index[-365:], y=gdf[v][-365:], mode='lines', name='Observed',
+                                                 line=dict(color='blue')), row=k + 1, col=1)
+                        fig.add_trace(go.Scatter(x=sim_df.index[-365:], y=sim_df[v][-365:], mode='lines',
+                                                 name='Synthesized', line=dict(color='red')), row=k + 1, col=1)
+                    fig.update_layout(height=800, width=1000, showlegend=True)
+                    fig.show()
 
                 result['stats'][var]['obs_dw'].append(obs_dw)
                 result['stats'][var]['sim_dw'].append(sim_dw)
@@ -127,6 +116,33 @@ def mc_timeseries_draw(station_meta, gridded, outdir, residuals_dir, station_typ
 
         with open(outfile, 'w') as f:
             json.dump(result, f, indent=4)
+
+
+def correlated_residual_sampling(residuals, model_estimates, n_samples):
+    n_time, n_vars = residuals.shape
+    perturbed_estimates = np.zeros((n_samples, n_time, n_vars))
+    correlated_residual_samples = empirical_copula_sample(residuals, n_samples)
+    for i in range(n_samples):
+        for j in range(n_vars):
+            perturbed_data = model_estimates.copy()
+            perturbed_data[:, j] = model_estimates[:, j] + correlated_residual_samples[i, j]
+            perturbed_estimates[i, :, :] = perturbed_data
+    return perturbed_estimates
+
+
+def empirical_copula_sample(data, n_samples):
+    n_time_steps, n_variables = data.shape
+    uniform_data = np.zeros_like(data, dtype=float)
+    for j in range(n_variables):
+        uniform_data[:, j] = rankdata(data[:, j]) / (n_time_steps + 1)
+    sample_indices = np.random.choice(n_time_steps, size=n_samples, replace=True)
+    uniform_samples = uniform_data[sample_indices, :]
+    samples = np.zeros_like(uniform_samples)
+    for j in range(n_variables):
+        for i in range(n_samples):
+            idx = np.argmin(np.abs(rankdata(data[:, j]) / (n_time_steps + 1) - uniform_samples[i, j]))
+            samples[i, j] = data[idx, j]
+    return samples
 
 
 def cross_autocorrelation_multivariate(df, lags=10, demean=True):
@@ -159,22 +175,24 @@ def cross_autocorrelation_multivariate(df, lags=10, demean=True):
     return results
 
 
-def variance_decomposition(sim_results, station_meta, decomp_out, station_type='ec'):
+def variance_decomposition(sim_dir, station_meta, decomp_out, station_type='ec', n_sample=100):
     kw = station_par_map(station_type)
-
-    with open(sim_results, 'r') as f:
-        sim_results = json.load(f)
 
     metvars = COMPARISON_VARS[:4]
     station_list = pd.read_csv(station_meta, index_col=kw['index'])
     var_sums = {k: 0. for k in metvars}
     all = 0.0
 
-    df = pd.DataFrame(index=list(sim_results.keys()), columns=metvars)
+    df = pd.DataFrame(index=list(station_list.index), columns=metvars)
 
     for j, (station, row) in enumerate(station_list.iterrows()):
 
-        if station not in sim_results.keys():
+        sim_results = os.path.join(sim_dir, f'eto_variance_{n_sample}_{station}.json')
+        if os.path.exists(sim_results):
+            with open(sim_results, 'r') as f:
+                sim_results = json.load(f)
+        else:
+            print(f'{os.path.basename(sim_results)} does not exist, skipping')
             continue
 
         station_var, station_sum = {}, 0.0
@@ -182,7 +200,7 @@ def variance_decomposition(sim_results, station_meta, decomp_out, station_type='
         for var in metvars:
             try:
 
-                sum_var = sum(np.array([i[1] for i in sim_results[station][var]]))
+                sum_var = sum(np.array([i[1] for i in sim_results[var]]))
 
                 station_var[var] = sum_var
                 df.loc[station, var] = sum_var
@@ -241,13 +259,13 @@ if __name__ == '__main__':
 
     # pandarallel.initialize(nb_workers=10)
 
-    num_sampl_ = 100
+    num_sampl_ = 1000
     variance_json = os.path.join(d, 'weather_station_data_processing', 'error_analysis', 'mc_par_variance')
     res_file_dir = os.path.join(d, 'weather_station_data_processing', 'comparison_data')
     mc_timeseries_draw(sta, grid_data, variance_json, res_file_dir, station_type='agri',
-                       num_samples=num_sampl_)
+                       num_samples=num_sampl_, overwrite=True)
 
-    decomp = os.path.join(d, 'weather_station_data_processing', 'error_analysis', 'var_decomp')
-    variance_decomposition(variance_json, sta, decomp, station_type='agri')
+    decomp = os.path.join(d, 'weather_station_data_processing', 'error_analysis', 'var_decomp_par.csv')
+    # variance_decomposition(variance_json, sta, decomp, station_type='agri', n_sample=num_sampl_)
 
 # ========================= EOF ====================================================================
